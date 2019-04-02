8f054d2998e4f24965a5b6abb0ea2731f9ae8d19
Christian <christian.luzzetti@gmail.com>
2019-04-02 17:43:03 +0200

    Added REST and Authorization in comments

--- 24th_YelpCamp_Project/v10 - Final/routes/comments.js
+++ 24th_YelpCamp_Project/v10 - Final/routes/comments.js
@@ -4,6 +4,8 @@ var router = express.Router({mergeParams: true});
 var Campground = require("../models/campground")
 var Comment = require("../models/comment");
 
+
+
 //  =====================
 //  COMMENTS ROUTES
 
@@ -48,11 +50,68 @@ router.post("/", isLoggedIn, (req, res) => {
     });
 });
 
+//EDIT - Route
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) {
+            res.redirect("/campgrounds");
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    })
+});
+
+//UPDATE - Route
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
+    console.log(req.params.comment_id);
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, theUpdatedComment) => {
+        if (err) {
+            consolg.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/"+ req.params.id);
+        }
+    });
+});
+
+//DESTROY - Route
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
+    Comment.findByIdAndDelete(req.params.comment_id, (err) => {
+        if (err) {
+            console.log("Impossibile eliminare il commento");
+            res.redirect("back");
+        } else {
+            console.log("Commento eliminato");
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
 };
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if(err) {
+                res.redirect("back");
+            } else {
+                //Does the user own the campground?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+        console.log("Not signed it");
+    }
+}
 
 module.exports = router;
